9e08e8bf3f35674489c648a68e78e0a80417d99d
Karinecasant <karinecasant@gmail.com>
2021-03-08 06:44:22 -0300

    Add files via upload

--- Aula6/src/main/java/org/generation/blogPessoal/repository/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.generation.blogPessoal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.generation.blogPessoal.model.ContatoModel;
-
-public interface ContatoRepository extends JpaRepository<ContatoModel, Long> {
-}
